7958bd85f60dd71dbdc88e7384eb5f6419bf973a
Kharaj Chakraborty <kharaj2007@gmail.com>
2026-01-03 13:43:52 +0530

    projects.jsx file optimized & formatted

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,27 +2,34 @@ import Card from "./components/Card"
 import "./css/Projects.css"
 
 const Projects = () => {
+
+  const project_details = [{
+    name: "Mind Matters",
+    lang: ["Kotlin", "HTML", "CSS", "JS"],
+    repo: `https://github.com/kharajch/Mind-Matters--Android`,
+    img: "Mind Matters.png",
+    details: `This interactive personality assessment quiz app features 17 questions with varying option formats
+                    designed to evaluate different aspects of your personality and behavior. The elegantly designed app
+                    presents questions in a modern gradient interface with smooth animations and progress tracking. As
+                    you answer each question, your selections accumulate points that ultimately categorize your results
+                    as Good, Moderate, or Severe, providing insight into your psychological patterns and tendencies. The
+                    responsive design ensures an engaging experience across all devices, complete with intuitive
+                    navigation and a restart option for repeated use.`,
+
+  }]
+
   return (
     <>
       <h1 className="heading" id="Projects">Projects</h1>
 
       <div className="projects_container hidden">
 
-        <Card img="Mind Matters.png" lang = {["Kotlin", "HTML", "CSS", "JS"]} name="Mind Matters" details={`This interactive personality assessment quiz app features 17 questions with varying option formats
-                    designed to evaluate different aspects of your personality and behavior. The elegantly designed app
-                    presents questions in a modern gradient interface with smooth animations and progress tracking. As
-                    you answer each question, your selections accumulate points that ultimately categorize your results
-                    as Good, Moderate, or Severe, providing insight into your psychological patterns and tendencies. The
-                    responsive design ensures an engaging experience across all devices, complete with intuitive
-                    navigation and a restart option for repeated use.`} repo={`https://github.com/kharajch/Mind-Matters--Android`} />
-
-        <Card img="c.jpg" lang = {["C Language"]} name="Reciept Generator" details={`This interactive personality assessment quiz app features 17 questions with varying option formats
-                    designed to evaluate different aspects of your personality and behavior. The elegantly designed app
-                    presents questions in a modern gradient interface with smooth animations and progress tracking. As
-                    you answer each question, your selections accumulate points that ultimately categorize your results
-                    as Good, Moderate, or Severe, providing insight into your psychological patterns and tendencies. The
-                    responsive design ensures an engaging experience across all devices, complete with intuitive
-                    navigation and a restart option for repeated use.`} repo={`https://github.com/kharajch/Mind-Matters--Android`} />
+        <Card 
+          name={project_details[0].name}
+          lang={project_details[0].lang}
+          repo={project_details[0].repo}
+          img={project_details[0].img}
+          details={project_details[0].details} />
 
       </div>
     </>
