2a518d4ae6d38b576f812a51d749ba71c52aec2a
kharajch <kharaj2007@gmail.com>
2026-01-14 16:55:50 +0530

    Next JS API Added

--- components/Contact.js
+++ components/Contact.js
@@ -1,16 +1,11 @@
 "use client"
 
-import { useState } from "react"
+import { useRef } from "react";
+import { HandleSubmit } from "./components/HandleSubmit";
+
 const Contact = () => {
 
-  const [Form, setForm] = useState([{
-    name: " ",
-    email: " ",
-    message: " "
-  }])
-  const handleChange = (e) => {
-    setForm({ ...Form, [e.target.name]: e.target.value });
-  }
+  const form_ref = useRef()
 
   return (
     <>
@@ -35,15 +30,15 @@ const Contact = () => {
 
 
         <div className="contact_form_container">
-          <form className="contact_form" method="POST" action={"https://kharajch-contact-form-submit-api-production.up.railway.app/"}>
+          <form className="contact_form" ref={form_ref} action={(e) => { HandleSubmit(e); form_ref.current.reset()}}>
             <label htmlFor="name">Name :</label>
-            <input type="text" id="name" name="name" value={Form.name} onChange={handleChange} required />
+            <input type="text" id="name" name="name" required />
 
             <label htmlFor="email">Email :</label>
-            <input type="text" id="email" name="email" value={Form.email} onChange={handleChange} required />
+            <input type="text" id="email" name="email" required />
 
             <label htmlFor="comment">Message :</label>
-            <textarea id="comment" rows={10} cols={39} name="message" value={Form.message} onChange={handleChange} required></textarea>
+            <textarea id="comment" rows={10} cols={39} name="message" required></textarea>
             <div className="submit_btn_container">
               <button id="submit_btn" type="submit">Send Message</button>
             </div>
